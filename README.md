3e9897142f21a425dfa2fdb97f130251dd10f82c
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2021-05-31 23:25:11 -0300

    First Commit and upload export to Microsoft Excel

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# xlsxexport
+# xlsxexport - Plataforma de visualização e exportação de arquivos em diversos formatos: XML, CSV e XSL (Microfost Excel), DOCX (Microsoft Word) e entre outros
 
-## Project setup
+## Project setup / Iniciando a aplicação
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Compiles and hot-reloads for development / Compilar em modo desenvovlimento
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
+### Compiles and minifies for production / Compilar em modo produção
 ```
 npm run build
 ```
 
-### Lints and fixes files
+### Lints and fixes files / Lint e arquivos fixos
 ```
 npm run lint
 ```
 
-### Customize configuration
+### Customize configuration / Customizando a configuração para VueJS 2
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <download-excel :data="json_data" :fields="json_fields" worksheet="work_sheet_name" name="excel_name" >
+      <button type="button" class="btn btn-success">
+        Exportar para o Excel
+      </button>
+    </download-excel>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data(){
+    return{ 
+      json_fields: {
+        "Registro": 'id',
+        "Nome": 'name',
+        "Sobrenome": 'surname',
+        "Idade": 'age',
+        "Telefone Residencial": 'phone.home',
+        "Telefone Celular": 'phone.work',
+        "Data de Registro":{
+          field:'birthDate',
+        },
+      },
+      json_data:[
+        {
+          id:1,
+          name:'Demostenes',
+          surname:'Brito',
+          age:90,
+          phone: {
+            home: '494933838',
+            work: '493780494',
+          },
+          birthDate:new Date(),
+        },
+        {
+          id:2,
+          name:'Demostenes',
+          surname:'Brito',
+          age:28,
+          phone: {
+            home: '349493838',
+            work: '780493494',
+          },
+          birthDate:new Date(),
+        },
+        {
+          id:3,
+          name:'Demostenes',
+          surname:'Brito',
+          age:32,
+          phone: {
+            home: '383832938',
+            work: '734949434',
+          },
+          birthDate:new Date(),
+        },
+      ]
+    }
   }
 }
+
 </script>
 
 <style>
